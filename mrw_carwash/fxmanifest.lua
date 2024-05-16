76bf5bf6c5d3fd265e589e03c2ec9126c498a7a8
MrWayne ♥ <136295538+MrWayne-Scripts@users.noreply.github.com>
2024-05-17 00:19:11 +0200

    Add files via upload

---
+++ mrw_carwash/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+description 'MrWayne - CarWash'
+author 'MrWayne'
+version '1.0.1'
+lua54 'yes'
+
+shared_scripts {
+	'config.lua',
+	'framework.lua',
+}
+
+client_scripts {
+	'client.lua',
+}
+
+server_scripts {
+	'server.lua',
+}
